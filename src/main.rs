1c2cf71053b0101a6df8649071f5aacae97bb599
willianjuliate <willianjuliate@gmail.com>
2021-10-10 17:24:50 -0300

    Finish exemple game

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+use std::process::Command;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
+    println!("###  Adivinhe o número!  ###");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failde ro read line");
-    println!("You guessed: {} ", guess);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    //println!("The secret number is: {} ", secret_number);
+
+    loop {
+        println!("Por favor, insira seu palpite.");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Falha ao ler a linha!"); // I/O Input and Output Keyboard
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Seu palpite: {} ", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Muito pequeno!"),
+            Ordering::Greater => println!("Muito grande!"),
+            Ordering::Equal => {
+                println!("Você ganhou!");
+                break;
+            }
+        }
+    }
+
+    let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
 }
